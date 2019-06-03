384c290f800591ee724d0fa11a8e04b844bd593a
ManoBalaje167 <44311168+ManoBalaje167@users.noreply.github.com>
2019-06-03 17:58:19 +0530

    Create Array_Rotation_Method_1.c

---
+++ Array_Rotation_Method_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void rotation(int arr[],int d,int n)
+{
+  int temp[d];
+  for(int i=0;i<d;i++)
+  {
+      temp[i]=arr[i];
+  }
+  for(int i=0;i<n-d;i++)
+  {
+    arr[i]=arr[i+d];
+  }
+  int j=0;
+  for(int i=n-d;i<n;i++)
+  {
+    arr[i]=temp[j++];
+  }
+  printf("The Arrays are \n");
+  for(int i=0;i<n;i++)
+  {
+    printf("%d\n",arr[i]);
+  }
+}
+
+int main(void) {
+  int arr[]={1,2,3,4,5,6,7};
+  int n=sizeof(arr)/sizeof(int);
+  printf("%d\n",n);
+  rotation(arr, 4, n);
+  return 0;
+}
